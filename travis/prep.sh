03caae8c76c5be1cf16ef8388a65b6cda5dbcc61
Dmytro Milinevskyy <milinevskyy@gmail.com>
2015-05-19 21:44:19 +0200

    travis: sanitize prep script

--- travis/prep.sh
+++ travis/prep.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 sleep 15
-cassandra-cli --batch < <(echo -e "create keyspace test with placement_strategy = 'org.apache.cassandra.locator.SimpleStrategy' and strategy_options = {replication_factor:1};\nuse test;\ncreate column family dcell;")
+
+cbf=$(mktemp)
+echo "create keyspace test with placement_strategy = 'org.apache.cassandra.locator.SimpleStrategy' and strategy_options = {replication_factor:1};" >> $cbf
+echo "use test;" >> $cbf
+echo "create column family dcell;" > $cbf
+cassandra-cli --batch < $cbf
